3d108621385e30017377a98066830ac86ad4d6ed
Juliansh787 <59464137+Juliansh787@users.noreply.github.com>
2021-05-31 16:13:52 +0900

    Delete sunEXE.py

--- sunEXE.py
+++
@@ -1,8 +0,0 @@
-from os import system
-
-# pyinstaller --noconsole --onefile --icon=../icons/sun.ico "sunEXE.py"
-
-try:
-    system('start dist\\SUN(GoogleImageScrapper)\\SUN(GoogleImageScrapper).exe')
-except:
-    pass
